22f3e01f615e6f426df0807b5c17eb4dcb0edf0c
Alexander <105300503+AlexWayke@users.noreply.github.com>
2024-09-16 00:01:37 +0300

    feat: added slider script 00:01 16/09/24

--- cssMemeSlider/script.js
+++ cssMemeSlider/script.js
@@ -1,1 +1,42 @@
-console.log('check')
+class Slider {
+  constructor(slider){
+    this.slider = document.querySelector(slider),
+    this.wrapper = this.slider.querySelector('.slider__wrapper'),
+    this.template = this.slider.querySelector('.slider__template'),
+    this.slides = this.slider.querySelectorAll('.slider__slide'),
+    this.tabs = this.slider.querySelector('.slider__tabs')
+  }
+
+  setWidth(){
+    let currentWidth = this.wrapper.offsetWidth;
+    let countOfSlides = Array.from(this.slides).length;
+    let activeSlide = this.tabs.querySelector('.active').dataset.order;
+
+    this.slides.forEach(slide => {
+      slide.style.width = `${currentWidth}px`;
+    });
+    this.template.style.width = `${currentWidth * countOfSlides}px`
+    this.template.style.transform = `translateX(${-currentWidth * activeSlide}px)`
+  }
+
+  slideTo(btn, id){
+    let currentWidth = this.wrapper.offsetWidth;
+    let activeBtn = this.tabs.querySelector('.active');
+    
+    activeBtn.classList.remove('active');
+    btn.classList.add('active');
+    this.template.style.transform = `translateX(${-currentWidth * id}px)`
+  }
+
+  showSlider(){
+    this.setWidth();
+  }
+}
+
+const slider = new Slider('.slider');
+
+slider.showSlider();
+
+window.addEventListener('resize', function() {
+  slider.setWidth();
+}, true);
